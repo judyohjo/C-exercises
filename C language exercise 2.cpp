558ad4073008848bb814800b1a3b9ed1be3e980b
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-12 00:29:18 +1200

    Add files via upload

---
+++ C language exercise 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int first, second;
+	printf("Input the first integer: ");
+	scanf("%d", &first);
+	printf("Input the second integer: ");
+	scanf("%d", &second);
+	printf("Sum of two integers = %d", first + second);
+}
+
+
+
+
