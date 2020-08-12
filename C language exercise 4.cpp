86048e0fc668d2505c2e1051f974b6b1eba3993a
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-13 00:18:51 +1200

    Add files via upload

---
+++ C language exercise 4.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	int first, second;
+	printf("Input the first integer: ");
+	scanf("%d", &first);
+	printf("Input the second integer: ");
+	scanf("%d", &second);
+	printf("Result = %d", first*second);
+
+}
+
+
+
+
