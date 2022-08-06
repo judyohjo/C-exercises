26b4de19045a3518c69c9959b49207fb5014fb63
Judy (Wonjae) Oh <68041382+judyohjo@users.noreply.github.com>
2022-08-06 19:51:04 +0900

    Create lab1q3.c

---
+++ lab1q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+    char str1[40];
+    char str2[40];
+    
+    scanf("%s", str1);
+    scanf("%s", str2);
+    
+    printf("The length of str1 is: %zu\n", strlen(str1));
+    printf("The length of str2 is: %zu\n", strlen(str2));
+    
+    strcat(str1, str2);
+    printf("%s\n", str1);
+
+    printf("The length of str1 is: %zu\n", strlen(str1));
+    printf("The length of str2 is: %zu", strlen(str2));
+    
+    
+    return 0;
+}
